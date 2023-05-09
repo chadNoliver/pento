794aa93def09a34a0bd8fcb2b9794817a23ffbf6
user <chadNoliver@gmail.com>
2023-05-09 11:43:36 -0500

    Check guess against answer for evaluation of successful win
    This completes the Give It a Try tasks and allows user to click a replay button after successful guess

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,17 +2,28 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+    {:ok, assign(socket, score: 0, message: "Make a guess: ", time: time(), answer: rand(), win: false)}
   end
 
   def handle_event("guess", %{"number" => guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
     time = time()
+    {message, score, win} = if (elem(Integer.parse(guess),0) == socket.assigns.answer) do
+      {"Your guess: #{guess}. Correct! ",
+        socket.assigns.score + 1,
+        true
+      }
+      
+    else
+      {"Your guess: #{guess}. Wrong. Guess again. ",
+        socket.assigns.score - 1,
+        false
+      }
+    end
     {
       :noreply,
       assign(
         socket,
+        win: win,
         time: time,
         message: message,
         score: score)}
@@ -24,15 +35,22 @@ defmodule PentoWeb.WrongLive do
     <h1>Your score: <%= @score %></h1>
     <h2>
       <%= @message %>
+    <br>
       It's <%= @time %>
     </h2>
+
     <h2>
+    <%= if assigns.win do %> 
+      <.link patch={~p"/guess"}>Replay?</.link>
+    <% else %>
       <%= for n <- 1..10 do%>
         <.link href="#" phx-click="guess" phx-value-number= {n} >
           <%= n%>
         </.link>
       <% end %>
+    <% end %>
     </h2>
+
     """
   end
 
@@ -40,4 +58,11 @@ defmodule PentoWeb.WrongLive do
     DateTime.utc_now |> to_string
   end
 
+  def rand() do
+    :rand.uniform(10)
+  end
+
+def handle_params(_params, _session, socket) do
+      {:noreply, assign(socket, score: 0, message: "Make a guess: ", time: time(), answer: rand(), win: false)}
+  end
 end
